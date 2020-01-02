dd1c5656a8377f0b9e5f6347893d4e79667062b3
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-02 05:16:08 -0800

    Write booklist and connect it to the store

---
+++ src/components/BookList.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactRedux from 'react-redux';
+
+class BookList extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    const {books} = this.props;
+    return(
+      <table>
+        <tr>
+          <th>Book Id</th>
+        </tr>
+        <tr>
+          <th>Title</th>
+        </tr>
+        <tr>
+          <th>Category</th>
+        </tr>
+        {books.map(book => 
+          <tr>
+            <td>{book.id}</td>
+            <td>{book.title}</td>
+            <td>{book.category}</td>
+          </tr>
+          )}
+        <tr></tr>
+      </table>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    books: state,
+  }
+}
+
+const mapDispatchToProps = () =>{
+  return{
+
+  }
+}
+
+const connect = ReactRedux.connect;
+
+connect(mapStateToProps, mapDispatchToProps)(BookList);
