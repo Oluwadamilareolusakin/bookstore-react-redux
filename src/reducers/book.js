5ad98809633ea5d9b33e87685a01e5760acddda3
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-03 20:48:58 +0100

    Combinde reducers

--- src/reducers/book.js
+++ src/reducers/book.js
@@ -1,12 +1,17 @@
-const ADD = 'ADD';
+const CREATE_BOOK = "CREATE_BOOK";
+const REMOVE_BOOK = "REMOVE_BOOK"
 
 const bookReducer = (state = [], action) => {
   switch(action.type){
-    case ADD:
+    case CREATE_BOOK:
       return [...state, action.book];
+    case REMOVE_BOOK:
+      const {targetBook} = action.book;
+      let newList = state.filter((book) => book != targetBook);
+      return newList;
     default:
       return state;
   }
-}
+};
 
 export default bookReducer;
