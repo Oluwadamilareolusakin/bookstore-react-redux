679d8f0f94b4ce8f2e8487f8a1d2efa81ad975c4
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-04 12:11:22 -0600

    Fix handleremove book call on button

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Book = (props) => {
-  const { book, key } = props;
+  const { book, key, handleRemoveBook } = props;
+  
   return (
     <tr key={key}>
       <td>{book.id}</td>
       <td>{book.title}</td>
       <td>{book.category}</td>
+      <td><button onClick={(event) => handleRemoveBook(event,book)}>Remove Book</button></td>
     </tr>
   );
 };
