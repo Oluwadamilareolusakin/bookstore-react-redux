2577ab23c29e2404a08cbe9cdbf6ad502371745f
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-04 12:14:45 -0600

    Write booklist component and container

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,38 +1,31 @@
 import React from 'react';
-import ReactRedux from 'react-redux';
 import Book from './Book';
-import { addBook, removeBook } from '../actions/book';
 
-const BookList = (props) => {
-  const { books } = props;
-  return (
-    <table>
-      <tr>
-        <th>Book Id</th>
-      </tr>
-      <tr>
-        <th>Title</th>
-      </tr>
-      <tr>
-        <th>Category</th>
-      </tr>
-      {books.map((book, index) => <Book book={book} key={index} />)}
-      <tr />
-    </table>
-  );
+class BookListComponent extends React.Component {  
+  handleRemoveBook = (event,book) => {
+    event.preventDefault();
+    const { deleteBook } = this.props;
+    deleteBook(book);
+  }
+
+  render() {
+    const { books }  = this.props;
+    return (
+      <table>
+        <tr>
+          <th>Book Id</th>
+        </tr>
+        <tr>
+          <th>Title</th>
+        </tr>
+        <tr>
+          <th>Category</th>
+        </tr>
+        {books.map((book, index) => <Book book={book} key={index} handleRemoveBook={(event) => this.handleRemoveBook(event,book)}/>)}
+        <tr />
+      </table>
+    );
+  }
 };
 
-const mapStateToProps = state => ({
-  books: state,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  addNewBook: book => dispatch(addBook(book)),
-  deleteBook: index => dispatch(removeBook(index)),
-});
-
-const { connect } = ReactRedux;
-
-connect(mapStateToProps, mapDispatchToProps)(BookList);
-
-export default BookList;
+export default BookListComponent;
