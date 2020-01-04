6e246e0a30da4464da1eba7bb2e34da1cf645e14
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-04 12:11:52 -0600

    Rewrite book form

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,16 +1,81 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addBook } from '../actions/book';
 
-const BookForm = (props) => {
-  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  return (
-    <form>
-      <input />
-      <select>
-        {categories.map(category, index => <option key={index}>{category}</option>))}
-      </select>
-      <button type="submit">Add Book</button>
-    </form>
-  );
+class BookForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      title: '',
+      category: '',
+    }
+  }
+
+  handleChange = (e) => {
+    if (e.target.id === 'title'){
+      const title = e.target.value;
+      this.setState({
+        title,
+      });
+      return;
+    }
+    const category = e.target.value;
+    this.setState({
+      category,
+    })
+  }
+
+  handleSubmit = (e, id) => {
+    e.preventDefault();
+    console.log(id.length);
+
+    const { title, category } = this.state;
+
+    const book = {
+      title,
+      category,
+      id,
+    }
+
+    this.props.addNewBook(book);
+
+    this.setState({
+      title: '',
+      category: '',
+    })
+  }
+
+  render() {
+    const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+    const { id } = this.props
+    return (
+      <form>
+        <input id="title" onChange={this.handleChange}/>
+        <select id="category" onChange={this.handleChange}>
+          {categories.map((category, index) => <option key={index}>{category}</option>)}
+        </select>
+        <button type="submit" onClick={(event) => this.handleSubmit(event, this.props.id)} >Add Book</button>
+      </form>
+    );
+  }
 };
+
+const mapStateToProps = (state) => {
+  const books = state.bookReducer;
+  const lastBook = books[books.length - 1];
+  const id = lastBook ? lastBook.id + 1 : 1;
+  return {
+    id
+  }
+}
+
+
+
+const mapDispatchToProps = (dispatch) => ({
+  addNewBook: book => dispatch(addBook(book)),
+});
+
+BookForm = connect(mapStateToProps, mapDispatchToProps)(BookForm);
+
 
 export default BookForm;
