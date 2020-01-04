8a6db78e96db9dae724a2a466145c9a9c489c7e1
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-04 12:15:57 -0600

    Add containers

---
+++ src/containers/BookList.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import BookListComponent from '../components/BookList';
+import { removeBook } from '../actions/book';
+
+
+const mapStateToProps = state => {
+  return {
+    books: state.bookReducer,
+  }
+};
+
+const mapDispatchToProps = dispatch => ({
+  deleteBook: book => dispatch(removeBook(book)),
+});
+
+const BookList = connect(mapStateToProps, mapDispatchToProps)(BookListComponent);
+
+export default BookList;
