ce82c7cda90eb0b9f82eca715f1fcc61182dc307
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-05 10:35:16 -0600

    Restructure book

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,12 +4,31 @@ const Book = (props) => {
   const { book, key, handleRemoveBook } = props;
   
   return (
-    <tr key={key}>
-      <td>{book.id}</td>
-      <td>{book.title}</td>
-      <td>{book.category}</td>
-      <td><button onClick={(event) => handleRemoveBook(event,book)}>Remove Book</button></td>
-    </tr>
+    <div key={key} className="book card row">
+      <div className="book-left">
+        <div className="book-details">
+          <h6 className="book-category">{book.category}</h6>
+          <div className="book-title">{book.title}</div>
+          <div className="book-author">By {book.author}</div>
+        </div>
+        <div className="book-controls">
+          <a onClick={(event) => handleRemoveBook(event,book)}>Remove</a>
+        </div>
+      </div>
+      <div className="book-right row">
+        <div className="book-completion">
+          <h5 className="heading">Progress</h5>
+          <div>
+            <h1 className="progress">{book.progress}%</h1>
+          </div>
+        </div>
+        <div className="book-details">
+          <h5 className="heading">Current Chapter</h5>
+          <p className="current-chapter">Chapter {book.currentChapter}</p>
+          <button>UPDATE PROGRESS</button>
+        </div>
+      </div>
+    </div>
   );
 };
 
