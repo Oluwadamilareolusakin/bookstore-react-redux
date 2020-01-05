c45f6811fe3cdc2cbfcf330318349a48ac4c254f
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-05 03:15:07 -0600

    Rewrite book reducer to use object assign function

--- src/reducers/book.js
+++ src/reducers/book.js
@@ -1,42 +1,45 @@
 const CREATE_BOOK = 'CREATE_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
 
-const initialState = [
-  {
-    title: 'The first placeholder book',
-    category: 'Action',
-    id: 1,
-  },
+const initialState = {
+  books: [
+    {
+      title: 'The first placeholder book',
+      category: 'Action',
+      id: 1,
+    },
 
-  {
-    title: 'The second placeholder book',
-    category: 'Action',
-    id: 2,
-  },
+    {
+      title: 'The second placeholder book',
+      category: 'Action',
+      id: 2,
+    },
 
-  {
-    title: 'The third placeholder book',
-    category: 'Action',
-    id: 3,
-  },
+    {
+      title: 'The third placeholder book',
+      category: 'Action',
+      id: 3,
+    },
 
-  {
-    title: 'The fourth placeholder book',
-    category: 'Action',
-    id: 4,
-  },
+    {
+      title: 'The fourth placeholder book',
+      category: 'Action',
+      id: 4,
+    },
 
-];
+  ]
+};
 
 
 const bookReducer = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_BOOK:
-      return [...state, action.book];
+      return Object.assign({}, state, {books: [...state.books, action.book]});
     case REMOVE_BOOK:
       const targetBook = action.book;
-      const newList = state.filter(book => book !== targetBook)
-      return newList;
+      const books = state.books;
+      const newList = books.filter(book => book !== targetBook)
+      return Object.assign({}, state, {books: newList});
     default:
       return state;
   }
