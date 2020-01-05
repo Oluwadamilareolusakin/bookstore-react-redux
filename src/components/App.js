37c04aa7adf418ab13ffbde7ce7a06880cbfac71
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-05 10:34:58 -0600

    Add node sass to project and render header in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BookList from '../containers/BookList';
 import BookForm from './BookForm';
+import Header from './Header';
 
 const App = (props) => {
   return(
   <div>
+    <Header/>
     <BookList />
     <BookForm />
   </div>
