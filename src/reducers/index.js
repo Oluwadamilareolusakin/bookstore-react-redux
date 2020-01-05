947a60313e4fb1823dc3d354b5ca57267651b9b7
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-05 03:15:38 -0600

    Combine filter reducer into root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import bookReducer from './book';
+import filterReducer from './filter';
 
 const rootReducer = combineReducers({
   bookReducer,
+  filterReducer,
 });
 
 export default rootReducer;
